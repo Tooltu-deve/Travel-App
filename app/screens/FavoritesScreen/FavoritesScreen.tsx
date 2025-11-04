3289c704031101cb1f30619e2a29e1dcdc40471b
tpGh-hub <24127161@student.hcmus.edu.vn>
2025-11-04 22:09:45 +0700

    style: update UI colors to match tailwind

---
+++ app/screens/FavoritesScreen/FavoritesScreen.tsx
@@ -0,0 +1,30 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { COLORS } from '../../../constants';
+
+const FavoritesScreen: React.FC = () => {
+  return (
+    <LinearGradient
+      colors={[COLORS.gradientStart, COLORS.gradientBlue1, COLORS.gradientBlue2, COLORS.gradientBlue3]}
+      locations={[0, 0.3, 0.6, 1]}
+      style={styles.container}
+    >
+      <Text style={styles.text}>Favorites Screen</Text>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: COLORS.textMain,
+  },
+});
+
+export default FavoritesScreen;
