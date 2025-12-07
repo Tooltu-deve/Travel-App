3997c2aba931a4497dc72ad81f06174e504c0c5a
unknown <ltlquang2420@clc.fitus.edu.vn>
2025-12-07 13:16:20 +0700

    Revert unrelated changes to match develop

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -22,8 +22,7 @@
  *   const API_BASE_URL = 'https://api.yourapp.com';
  */
 // const API_BASE_URL = 'https://travel-app-r9qu.onrender.com'; // ⬅️ Render Cloud URL
-const API_BASE_URL = 'http://localhost:3000'; // ⬅️ Render Cloud URL
-
+const API_BASE_URL = 'http://10.0.2.2:3000';
 // ============================================
 // TYPES
 // ============================================
